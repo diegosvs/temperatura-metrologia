dc6fd8fd6d1784ee81e917b69af3c2013af2059a
diegosvs <diegovisantos@gmail.com>
2019-06-12 17:03:02 -0300

    parametros de comunicação 9142

--- códigos-teste/python e QT/funcAgilent.py
+++ códigos-teste/python e QT/funcAgilent.py
@@ -1,5 +1,5 @@
 import visa
-from PyQt5 import QtCore, QtGui
+from visa import constants
 import time, csv, os, _thread as t
 
 def conectarMultimetroAgilent():
@@ -11,10 +11,18 @@ def conectarMultimetroAgilent():
     
     global multimetro,b
 
+       
+
+
     try:
         
-        multimetro=portas_conectadas.open_resource(instrumentos__conectados[0]) #conecta o multimetro agilent U3606A pela GPIB
-
+        #multimetro=portas_conectadas.open_resource(instrumentos__conectados[0]) #conecta o multimetro agilent U3606A pela GPIB
+        multimetro=portas_conectadas.open_resource('ASRL4::INSTR',
+                                                   parity=constants.Parity.none,
+                                                   data_bits=8,
+                                                   baud_rate=9600,
+                                                   stop_bits=constants.StopBits.one
+                                                   )
         #print(multimetro.query("*IDN?")) #pede a identificacao do multimetro e imprime na tela
         #multimetro.write("F1")
         #multimetro.write("SW0")
